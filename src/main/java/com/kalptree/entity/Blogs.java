b07a6acb228b477965a5352507d4f689f639c4b2
kalp-vora <kalp.vora09@gmail.com>
2022-10-11 16:10:36 +0530

    Blogs added

---
+++ src/main/java/com/kalptree/entity/Blogs.java
@@ -0,0 +1,71 @@
+package com.kalptree.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Blogs {
+
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
+    private Long blogId;
+
+    @NotEmpty(message = "Please enter a title")
+    @Column(nullable = false)
+    private String title;
+
+    @NotEmpty(message = "Please enter some content")
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @NotEmpty(message = "Please enter a publish date and time")
+    @Column(nullable = false)
+    private LocalDateTime publishDateTime;
+
+    @Value("${default.penName: Anonymous}")
+    @Column(nullable = false, length = 50, columnDefinition = "varchar(255) default 'Anonymous' ")
+    private String penName;
+
+    @Value("${default.isPublished: false}")
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean isPublished;
+
+    @Value("${default.likes: #{0}}")
+    @Column(nullable = false, columnDefinition = "bigint default 0")
+    private Long likeCount;
+
+    @Value("${default.funny: #{0}}")
+    @Column(nullable = false, columnDefinition = "bigint default 0")
+    private Long funnyCount;
+
+    @Value("${default.insightful: #{0}}")
+    @Column(nullable = false, columnDefinition = "bigint default 0")
+    private Long insightfulCount;
+
+    @ManyToOne
+    @JoinColumn(name = "category", referencedColumnName = "categoryId")
+    private Categories category;
+
+    @ManyToOne
+    @JoinColumn(name = "user", referencedColumnName = "userId")
+    private Users user;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime creationDateTime;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedDateTime;
+}
