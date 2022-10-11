8cf57cb13493a9b849cc0659c594c7d62f32cf85
kalp-vora <kalp.vora09@gmail.com>
2022-10-11 15:37:09 +0530

    Categories added

---
+++ src/main/java/com/kalptree/entity/Categories.java
@@ -0,0 +1,31 @@
+package com.kalptree.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Categories {
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, updatable = false)
+    private Integer categoryId;
+
+    @NotEmpty(message = "Please enter a category name")
+    @Column(nullable = false, unique = true)
+    private String categoryName;
+
+    @CreationTimestamp
+    private LocalDateTime creationDateTime;
+}
