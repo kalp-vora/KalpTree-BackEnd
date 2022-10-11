df084d6f138d224c121f1d0caeab772443e049cc
kalp-vora <kalp.vora09@gmail.com>
2022-10-11 15:29:38 +0530

    Initial commit

---
+++ src/main/java/com/kalptree/KalpTreeApplication.java
@@ -0,0 +1,13 @@
+package com.kalptree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalpTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalpTreeApplication.class, args);
+	}
+
+}
